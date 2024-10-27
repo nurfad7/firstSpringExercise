50dcabfe96fb63a8ca9efbf1ff21ca66bfcbc48f
nurfad7 <nurfad7@gmail.com>
2024-10-27 21:05:00 +0700

    fix: path to favorite-products API

--- src/main/java/com/nurfad/firstSpringExercise/controller/FavoriteProductController.java
+++ src/main/java/com/nurfad/firstSpringExercise/controller/FavoriteProductController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/products")
+@RequestMapping("/api/v1/favorite-products")
 public class FavoriteProductController {
     private final FavoriteProductService favoriteProductService;
 
